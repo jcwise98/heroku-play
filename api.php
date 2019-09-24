33cdcfb91f7d8ca8921f78da388cadecf9c97ada
Jacob Wise <jacob.wise@herrs.com>
2019-09-24 14:30:39 -0400

    combo of words

--- api.php
+++ api.php
@@ -8,7 +8,7 @@
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE length=6 and weight <= 10 order by random() limit 0, 1";
+    $query = "SELECT rack FROM racks WHERE length=6 and weight <= 10 order by random() limit 0, 1";
     //$query2 = "SELECT words FROM racks WHERE length <=6"
     //$query = "SELECT rack, words FROM racks WHERE length=6 and weight <= 10 order by random() limit 0, 1";
     
@@ -23,12 +23,32 @@
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     
+    $rack = $results[0]['rack'];
+    
+    $query2 = "SELECT rack, words FROM racks where rack= :somerack";
+    
+    $statement2 = $dbhandle->prepare($query);
+    $statement2->bindValue(':somerack', $rack);
+    $statement2->execute();
+    $results2 = $statement2->fetchAll(PDO::FETCH_ASSOC);
+    
+    $combArray = array(substr($results[0]['rack'],0, 6), substr($results[0]['rack'],0, 5), substr($results[0]['rack'],0, 4), substr($results[0]['rack'],0, 3), substr($results[0]['rack'],0, 2));
+    
+    for($i=0; $i<=count($combArray); $i++) {
+      $queryArr = "SELECT rack, words FROM racks where rack= :somerack";
+      $statement3 = $dbhandle->prepare($queryArr);
+      $statement3->bindValue(':somerack', $combArray[$i]);
+      $statement3->execute();
+      $results3 = $statement3->fetchAll(PDO::FETCH_ASSOC);
+      $results2=array_merge($results2, $results3);
+    }
+    
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
     //making to this line means everything was great with this request
     header('HTTP/1.1 200 OK');
     //this lets the browser know to expect json
     header('Content-Type: application/json');
     //this creates json and gives it back to the browser
-    echo json_encode($results);
+    echo json_encode($results2);
 
 ?>
